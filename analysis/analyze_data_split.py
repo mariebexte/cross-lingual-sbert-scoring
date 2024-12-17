5f9ac2587bbd1574e0b1b828a02c5e991547dd32
mariebexte <mariebexte@gmail.com>
2024-12-17 13:16:34 +0100

    widen data analysis to include all datasets

--- analysis/analyze_data_split.py
+++ analysis/analyze_data_split.py
@@ -1,57 +1,68 @@
 import pandas as pd
 import os
 
-
-# Analyze data split of ePIRLS data
-data_folder = '/data/exp'
-
-results = {} 
-results_idx = 0
-
-frequencies = {}
-
-for prompt in os.listdir(data_folder):
-
-    for language in os.listdir(os.path.join(data_folder, prompt)):
-
-        df_train = pd.read_csv(os.path.join(data_folder, prompt, language, 'train.csv'))
-        df_val = pd.read_csv(os.path.join(data_folder, prompt, language, 'val.csv'))
-        df_test = pd.read_csv(os.path.join(data_folder, prompt, language, 'test.csv'))
-
-        df_full = pd.concat([df_train, df_val, df_test])
-
-        avg_len = 0
-        all_answers = list(df_full['Value'])
-
-        for answer in all_answers:
-
-            avg_len += len(str(answer))
-        
-        avg_len = avg_len/len(df_full)
-
-        all_answers_set = set(all_answers)
-
-        ratio_unique = len(all_answers_set)/len(all_answers)
-
-        results[results_idx] = {'prompt': prompt, 'lang': language, 'avg_len': avg_len, 'ratio_unique': ratio_unique}
-        results_idx += 1
-
-        if language == 'ar':
-
-            score_dist = dict(df_full['score'].value_counts())
-            total = sum(list(score_dist.values()))
-            score_dist_percentage = {score: freq/total for score, freq in score_dist.items()}
-            # print(prompt, language, dict(df_full['score'].value_counts()), score_dist_percentage)
-            frequencies[prompt] = score_dist_percentage
+from config import EPIRLS, ASAP_T, ASAP_M
 
 
-df_results = pd.DataFrame.from_dict(results, orient='index')
-print(df_results)
+def process_dataset(data_folder, dataset_name, languages, answer_column):
 
-for lang, df_lang in df_results.groupby('lang'):
+    results = {} 
+    results_idx = 0
 
-    print(lang, df_lang['avg_len'].mean(), df_lang['ratio_unique'].mean(), len(df_lang))
+    frequencies = {}
 
-df_frequencies = pd.DataFrame(frequencies).T
-# print(df_frequencies)
-df_frequencies.to_csv('/data/label_distributions_exp.csv')
+    for prompt in os.listdir(data_folder):
+
+        for language in languages:
+
+            df_train = pd.read_csv(os.path.join(data_folder, prompt, language, 'train.csv'))
+            df_val = pd.read_csv(os.path.join(data_folder, prompt, language, 'val.csv'))
+            df_test = pd.read_csv(os.path.join(data_folder, prompt, language, 'test.csv'))
+
+            df_full = pd.concat([df_train, df_val, df_test])
+
+            avg_len = 0
+            all_answers = list(df_full[answer_column])
+
+            for answer in all_answers:
+
+                avg_len += len(str(answer))
+            
+            avg_len = avg_len/len(df_full)
+
+            all_answers_set = set(all_answers)
+
+            ratio_unique = len(all_answers_set)/len(all_answers)
+
+            results[results_idx] = {'prompt': prompt, 'lang': language, 'avg_len': avg_len, 'ratio_unique': ratio_unique}
+            results_idx += 1
+
+            # Exemplary processing of first language (score distributions are balanced)
+            if language == languages[0]:
+
+                score_dist = dict(df_full['score'].value_counts())
+                total = sum(list(score_dist.values()))
+                score_dist_percentage = {score: freq/total for score, freq in score_dist.items()}
+                # print(prompt, language, dict(df_full['score'].value_counts()), score_dist_percentage)
+                frequencies[prompt] = score_dist_percentage
+
+
+    df_results = pd.DataFrame.from_dict(results, orient='index')
+
+    with open('/data/' + dataset_name + '_answer_stats.tsv', 'w') as out_file:
+
+        out_file.write('language\tavg_len\tratio_unique\tlength\n')
+
+        for lang, df_lang in df_results.groupby('lang'):
+
+            print(lang, df_lang['avg_len'].mean(), df_lang['ratio_unique'].mean(), len(df_lang))
+            out_file.write(lang+'\t'+str(df_lang['avg_len'].mean())+'\t'+str(df_lang['ratio_unique'].mean())+'\t'+str(len(df_lang)))
+
+    df_frequencies = pd.DataFrame(frequencies).T
+    df_frequencies.to_csv('/data/' + dataset_name + '_scores.csv')
+
+
+# ePIRLS
+for dataset in [EPIRLS, ASAP_T, ASAP_M]:
+
+    process_dataset(data_folder=dataset['dataset_path'], dataset_name=dataset['dataset_name'], languages=dataset['languages'], answer_column=dataset['answer_column'])
